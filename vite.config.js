4a40af7c04f569c8554de471c2eeb7109c76f287
JamesCalingo <james.calingo.web@gmail.com>
2024-05-17 10:48:35 -0400

    manifest.js

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
-const manifest = {
-  "registerType": "prompt",
-  "includeAssests": ["favicon.ico", "apple-touch-icon.png", "favicon/android-chrome-192x192.png", "favicon/android-chrome-384x384.png"],
-  "manifest": {
-    "name": "GogoaT",
-    "short_name": "GogoaT",
-    "description": "Find your train...instantly!",
-    "icons": [
-      {
-        "src": "favicon/android-chrome-192x192.png",
-        "sizes": "192x192",
-        "type": "image/png",
-        "purpose": "favicon"
-      },
-      {
-        "src": "favicon/android-chrome-384x384.png",
-        "sizes": "512x512",
-        "type": "image/png",
-        "purpose": "favicon"
-      },
-      {
-        "src": "favicon/apple-touch-icon.png",
-        "sizes": "180x180",
-        "type": "image/png",
-        "purpose": "apple touch icon"
-      }
-    ],
-    "theme_color": "#171717",
-    "background_color": "#f0e7db",
-    "display": "standalone",
-    "scope": "/",
-    "start_url": "/",
-    "orientation": "portrait"
-  }
-}
+import { manifest } from "./manifest.js"
+
 
 
 // https://vitejs.dev/config/
