350087f166d140f98a101915ed24d06e6274d8ac
JamesCalingo <james.calingo.web@gmail.com>
2024-05-17 12:41:48 -0400

    revert

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import { manifest } from "./manifest.json"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({
-    injectRegister: "auto",
     manifest: manifest
   })],
 })
