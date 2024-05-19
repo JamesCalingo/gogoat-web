8c953fb2374106bee6093389274451ad2e8546c4
JamesCalingo <james.calingo.web@gmail.com>
2024-05-18 23:49:21 -0400

    fixing manifest stuff

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
-import { manifest } from "./manifest.json"
+import { options } from "./manifest.json"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [react(), VitePWA({
-    manifest
-  })],
+  plugins: [react(), VitePWA(options)]
 })
