1e4a68c98cf2e5b697e059f837fa4ec55c5a099f
JamesCalingo <james.calingo.web@gmail.com>
2024-05-29 17:33:29 -0400

    adding webmanfiest

--- vite.config.js
+++ vite.config.js
@@ -1,55 +1,55 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
-// import { options } from "./manifest.json"
+import { options } from "./manifest.json"
 
-const options = {
-    registerType: "prompt",
-    includeAssests: [
-      "favicon.ico",
-      "apple-touch-icon.png",
-      "android-chrome-192x192.png",
-      "android-chrome-384x384.png",
-      "masked-icon.svg"
-    ],
-    manifest: {
-      name: "GogoaT",
-      short_name: "GogoaT",
-      description: "Find your train...instantly!",
-      icons: [
-        {
-          src: "/android-chrome-192x192.png",
-          sizes: "192x192",
-          type: "image/png",
-          purpose: "favicon"
-        },
-        {
-          src: "/android-chrome-384x384.png",
-          sizes: "512x512",
-          type: "image/png",
-          purpose: "favicon"
-        },
-        {
-          src: "/apple-touch-icon.png",
-          sizes: "180x180",
-          type: "image/png",
-          purpose: "apple touch icon"
-        },
-        {
-          src: "/maskable_icon.png",
-          sizes: "225x225",
-          type: "image/png",
-          purpose: "any maskable"
-        }
-      ],
-      theme_color: "#171717",
-      background_color: "#f0e7db",
-      display: "standalone",
-      scope: "/",
-      start_url: "/",
-      orientation: "portrait"
-    }
-  }
+// const options = {
+//     registerType: "autoUpdate",
+//     includeAssests: [
+//       "favicon.ico",
+//       "apple-touch-icon.png",
+//       "android-chrome-192x192.png",
+//       "android-chrome-384x384.png",
+//       "masked-icon.svg"
+//     ],
+//     manifest: {
+//       name: "GogoaT",
+//       short_name: "GogoaT",
+//       description: "Find your train...instantly!",
+//       icons: [
+//         {
+//           src: "/android-chrome-192x192.png",
+//           sizes: "192x192",
+//           type: "image/png",
+//           purpose: "favicon"
+//         },
+//         {
+//           src: "/android-chrome-384x384.png",
+//           sizes: "512x512",
+//           type: "image/png",
+//           purpose: "favicon"
+//         },
+//         {
+//           src: "/apple-touch-icon.png",
+//           sizes: "180x180",
+//           type: "image/png",
+//           purpose: "apple touch icon"
+//         },
+//         {
+//           src: "/maskable_icon.png",
+//           sizes: "225x225",
+//           type: "image/png",
+//           purpose: "any maskable"
+//         }
+//       ],
+//       theme_color: "#171717",
+//       background_color: "#f0e7db",
+//       display: "standalone",
+//       scope: "/",
+//       start_url: "/",
+//       orientation: "portrait"
+//     }
+//   }
 
 
 
